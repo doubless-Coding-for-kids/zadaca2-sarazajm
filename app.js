6578d73c22dcff3572d9ab2f6ca3a71239fcad41
sarazajm <sarazajmovic2@gmail.com>
2024-01-06 17:46:43 +0100

    Zadatak 2

---
+++ app.js
@@ -0,0 +1,8 @@
+//Zadatak 2
+let pozdravi = ["Zdravo","Pozdrav","Dobar dan","Dobro jutro","Dobra vece","Postovanje","Dovidenja","Ciao","Selam alejkum","Hi"];
+
+function ispisi(niz){
+    for (let i =0; i<8; i++){console.log(niz[i])};
+};
+
+ispisi(pozdravi);
